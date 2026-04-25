1ea200c93702a5822ac25cc908228ecdbcbfe2d2
Waqas raza <waqasraza1208@gmail.com>
2026-04-25 13:52:04 +0500

    1st video complete

---
+++ FirstApp/src/Hello.jsx
@@ -0,0 +1,8 @@
+
+import './index.css'
+function Hello(){
+    const name ="Waqas Raza"
+    return <h1>Hello world {name}</h1>
+}
+
+export default Hello;
---
+++ FirstApp/src/Navbar.jsx
@@ -0,0 +1,6 @@
+
+
+function Navbar(){
+    return <h1>This is Hello Navebar</h1>
+}
+export default Navbar;
